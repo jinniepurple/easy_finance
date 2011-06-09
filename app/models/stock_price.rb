ac4dea5e68ba07280abf046ec11fcb38126993ec
jinniepurple <jinniepurple@gmail.com>
2011-06-09 00:03:25 -0400

    merged http branch

--- app/models/stock_price.rb
+++ app/models/stock_price.rb
@@ -16,14 +16,10 @@ class StockPrice
   def get_stock_price
 
     # Uncomment these three lines and remove the other lines from this method and getting the stock price should work.
-    # response = HTTParty.get('http://finance.google.com/finance/info?client=ig&q=NASDAQ%3aAAPL')
-    # stock_hash = parse_gfinance_response(response)
-    # stock_hash["l"].to_f
-    
-    # Remove these lines
-    body = '[ { "id": "358464" ,"t" : "MSFT" ,"e" : "NASDAQ" ,"l" : "24.76" ,"l_cur" : "24.76" ,"s": "0" ,"ltt":"11:29AM EDT" ,"lt" : "May 27, 11:29AM EDT" ,"c" : "+0.09" ,"cp" : "0.35" ,"ccol" : "chg" } ]'
-    #body = res.body.gsub(/^\/\//, "")
-    ActiveSupport::JSON.decode(body).first["l"].to_f
+    response = HTTParty.get(stock_price_url)
+    stock_hash = parse_gfinance_response(response)
+    stock_hash["l"].to_f
+
 
   end
   
